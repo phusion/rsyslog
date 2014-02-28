d8d200008c0170408c6a8cffadb696226c0f7b6f
Bernhard K. Weisshuhn <bkw@codingforce.com>
2014-02-27 22:14:52 -0500

    [COOK-4126] rsyslog cookbook fails restarts due to not using upstart
    Signed-off-by: Sean OMeara <someara@opscode.com>
    
    Conflicts:
    	recipes/default.rb

--- recipes/default.rb
+++ recipes/default.rb
@@ -90,8 +90,15 @@ if platform_family?('omnios')
 end
 
 if !has_service_spec
+  if node['platform'] == 'ubuntu' && node['platform_version'].to_f >= 12.04
+     service_provider = Chef::Provider::Service::Upstart
+  else
+     service_provider = nil
+  end
+
   service node['rsyslog']['service_name'] do
     supports :restart => true, :reload => true, :status => true
     action   [:enable, :start]
+    provider service_provider
   end
 end
